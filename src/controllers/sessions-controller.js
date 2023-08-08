319f5c5b74088243ea7299bf4c44eb8c26cbc005
rayaneacacio <rray74307@gmail.com>
2023-08-08 17:44:28 -0300

    separando a lógica para remover o acesso do controller ao banco de dados

--- src/controllers/sessions-controller.js
+++ src/controllers/sessions-controller.js
@@ -1,28 +1,21 @@
-const knex = require("../database/knex");
-const authConfig = require("../configs/auth");
-const { sign } = require("jsonwebtoken");
-const { compare } = require("bcryptjs");
-const AppError = require("../utils/AppError");
+const SessionsRepository = require("../repositories/SessionsRepository");
+const SessionService = require("../services/session-services/SessionService");
 
 class SessionsController{
   async create(request, response){
     const { email, password } = request.body;
 
-    const user = await knex("users").where({ email }).first();
+    const sessionsRepository = new SessionsRepository;
+    const user = await sessionsRepository.findUserByEmail({ email });
 
-    if(!user) {
-      throw new AppError("email e/ou senha incorreta", 401);
+    const sessionService = new SessionService(sessionsRepository);
+    const emailExist = await sessionService.validateEmail({ user });
+
+    let token = null;
+
+    if(emailExist) {
+      token = await sessionService.comparePassword({ user, password });
     }
-
-    if(!(await compare(password, user.password))) {
-      throw new AppError("email e/ou senha incorreta", 401);
-    }
-
-    const { secret, expiresIn } = authConfig.jwt;
-    const token = sign({}, secret, {
-      subject: String(user.id),
-      expiresIn
-    })
 
     return response.json({ user, token });
   }
